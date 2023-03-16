0400cfd268636590fe5d43dbc4d39c8689ed6ee9
Tafnes Silva Barbosa <tafnessilvabarbosa@gmail.com>
2023-03-16 10:51:04 -0300

    Greedy and A* created

--- path_planner.py
+++ path_planner.py
@@ -73,6 +73,7 @@ class PathPlanner(object):
                         successor.f = node.f + self.cost_map.get_edge_cost((i_node, j_node), successor_position)
                         successor.parent = node
                         heapq.heappush(pq, (successor.f, successor))
+        return [], inf
 
     def greedy(self, start_position, goal_position):
         """
@@ -89,6 +90,31 @@ class PathPlanner(object):
 		# The first return is the path as sequence of tuples (as returned by the method construct_path())
 		# The second return is the cost of the path
         self.node_grid.reset()
+
+        goal_node = self.node_grid.get_node(goal_position[0], goal_position[1])
+
+        pq = [] # Creates the heap
+        node = self.node_grid.get_node(start_position[0], start_position[1]) # Get start node
+        node.f = node.distance_to(goal_position[0], goal_position[1]) # Set first cost
+        node.g = 0
+        heapq.heappush(pq, (node.f, node)) # Insert the first node
+        while len(pq) != 0:
+            f, node = heapq.heappop(pq) # Extract priority node
+            if not node.closed: # Verifies if node was already closed
+                node.closed = True
+
+                i_node, j_node = node.get_position() # Get position of priority node
+                for successor_position in self.node_grid.get_successors(i_node, j_node): # For all positions of successor of priority node
+                    successor = self.node_grid.get_node(successor_position[0], successor_position[1]) # Get successor node
+                    # Verifies if the cost of getting to the node is greater than the update.
+                    # Working with graphs and not trees, avoid cycles
+                    if successor.g > node.g + self.cost_map.get_edge_cost((i_node, j_node), successor_position):
+                        successor.parent = node
+                        successor.g = node.g + self.cost_map.get_edge_cost((i_node, j_node), successor_position)
+                        if successor == goal_node: # If node was excracted of heap and is the goal node, return
+                            return self.construct_path(goal_node), goal_node.g
+                        successor.f = successor.distance_to(goal_position[0], goal_position[1])
+                        heapq.heappush(pq, (successor.f, successor))
         return [], inf
 
     def a_star(self, start_position, goal_position):
@@ -106,4 +132,31 @@ class PathPlanner(object):
 		# The first return is the path as sequence of tuples (as returned by the method construct_path())
 		# The second return is the cost of the path
         self.node_grid.reset()
+
+        goal_node = self.node_grid.get_node(goal_position[0], goal_position[1])
+
+        pq = [] # Creates the heap
+        node = self.node_grid.get_node(start_position[0], start_position[1]) # Get start node
+        node.g = 0
+        node.f = node.distance_to(goal_position[0], goal_position[1]) # Set first cost
+        
+        heapq.heappush(pq, (node.f, node)) # Insert the first node
+        while len(pq) != 0:
+            f, node = heapq.heappop(pq) # Extract priority node
+            if not node.closed: # Verifies if node was already closed
+                node.closed = True
+
+                if node == goal_node: # If node was excracted of heap and is the goal node, return
+                    return self.construct_path(goal_node), goal_node.g
+
+                i_node, j_node = node.get_position() # Get position of priority node
+                for successor_position in self.node_grid.get_successors(i_node, j_node): # For all positions of successor of priority node
+                    successor = self.node_grid.get_node(successor_position[0], successor_position[1]) # Get successor node
+                    # Verifies if the cost of getting to the node is greater than the update.
+                    # Working with graphs and not trees, avoid cycles
+                    if successor.f > node.g + self.cost_map.get_edge_cost((i_node, j_node), successor_position) + successor.distance_to(goal_position[0], goal_position[1]):
+                        successor.g = node.g + self.cost_map.get_edge_cost((i_node, j_node), successor_position)
+                        successor.f = successor.g + successor.distance_to(goal_position[0], goal_position[1])
+                        successor.parent = node
+                        heapq.heappush(pq, (successor.f, successor))
         return [], inf
